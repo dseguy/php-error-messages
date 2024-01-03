4a00c0b9c800a26aee4621abf344e83b4000c68f
Damien Seguy <dseguy@exakat.io>
2024-01-03 10:35:10 +0100

    tool to create messages confortably

---
+++ scripts/create.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($argv[2])) {
+	$message = implode(' ', array_slice($argv, 1));
+} else {
+	$message = $argv[1];
+}
+
+print "Creating entry for '$message'\n";
+
+$name = strtolower($message);
+$name = str_replace(' ', '-', $name);
+
+if (file_exists('errors/'.$name.'.ini')) {
+	print "There is already an entry for 'docs/$name.ini'. Aborting\n";
+	die();
+}
+
+$ini = file_get_contents('errors/skeleton.ini');
+$ini = str_replace('id = ""', 'id = "'.$name.'"', $ini);
+$ini = str_replace('error = ""', 'error = "'.$message.'"', $ini);
+$ini = str_replace('', '', $ini);
+
+// @todo : also add a call to getMessage to find more details about the error message
+//print shell_exec('cd ../../bin/; php getMessage "'.$message.'"');
+
+file_put_contents('errors/'.$name.'.ini', $ini);
+
+print "New entry for 'errors/$name.ini'.\n";
+
+?>
